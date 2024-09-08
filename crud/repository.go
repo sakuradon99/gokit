6e824177d5a79e034fd5a3f3221d1ffce7a31734
SakuraDon <sakuradon99@outlook.com>
2024-09-08 15:41:26 +0800

    update crud

--- crud/repository.go
+++ crud/repository.go
@@ -3,71 +3,96 @@ package crud
 import (
 	"context"
 	"github.com/sakuradon99/gokit/db"
-	"gorm.io/gorm/clause"
+	"gorm.io/gorm"
 )
 
+type DBOption func(db *gorm.DB) *gorm.DB
+
 type Repository[T any] interface {
-	Insert(ctx context.Context, entity T) error
-	DeleteByID(ctx context.Context, id any, options ...RemoveOption) error
-	DeleteByIDs(ctx context.Context, ids []any, options ...RemoveOption) error
-	UpdateByID(ctx context.Context, entity T) error
-	SelectByID(ctx context.Context, id any, options ...QueryOption) (T, error)
-	SelectByIDs(ctx context.Context, ids []any, options ...QueryOption) ([]T, error)
+	Insert(ctx context.Context, entity T, options ...DBOption) error
+	InsertBatch(ctx context.Context, entities []T, options ...DBOption) error
+	Delete(ctx context.Context, condition T, options ...DBOption) error
+	DeleteByID(ctx context.Context, id any, options ...DBOption) error
+	DeleteByIDs(ctx context.Context, ids []any, options ...DBOption) error
+	Update(ctx context.Context, entity T, options ...DBOption) error
+	UpdateBatch(ctx context.Context, entities []T, options ...DBOption) error
+	SelectOne(ctx context.Context, condition T, options ...DBOption) (T, error)
+	SelectList(ctx context.Context, condition T, options ...DBOption) ([]T, error)
+	SelectByID(ctx context.Context, id any, options ...DBOption) (T, error)
+	SelectByIDs(ctx context.Context, ids []any, options ...DBOption) ([]T, error)
+	SelectCount(ctx context.Context, condition T, options ...DBOption) (int64, error)
 }
 
 type RepositoryImpl[T any] struct {
 	dbm db.Manager `inject:""`
 }
 
-func (r *RepositoryImpl[T]) Insert(ctx context.Context, entity T) error {
-	return r.dbm.DB(ctx).Create(&entity).Error
-}
-
-func (r *RepositoryImpl[T]) DeleteByID(ctx context.Context, id any, options ...RemoveOption) error {
-	opt := buildRemoveOptions(options...)
-	d := r.dbm.DB(ctx)
-	if opt.RemoveAssociations {
-		d = d.Select(clause.Associations)
+func (r *RepositoryImpl[T]) db(ctx context.Context, options ...DBOption) *gorm.DB {
+	gdb := r.dbm.DB(ctx)
+	for _, option := range options {
+		gdb = option(gdb)
 	}
-
-	var entity T
-	return r.dbm.DB(ctx).Delete(&entity, id).Error
+	return gdb
 }
 
-func (r *RepositoryImpl[T]) DeleteByIDs(ctx context.Context, ids []any, options ...RemoveOption) error {
-	opt := buildRemoveOptions(options...)
-	d := r.dbm.DB(ctx)
-	if opt.RemoveAssociations {
-		d = d.Select(clause.Associations)
-	}
-
-	var entity T
-	return r.dbm.DB(ctx).Delete(&entity, ids).Error
+func (r *RepositoryImpl[T]) Insert(ctx context.Context, entity T, options ...DBOption) error {
+	return r.db(ctx, options...).Create(&entity).Error
 }
 
-func (r *RepositoryImpl[T]) UpdateByID(ctx context.Context, entity T) error {
-	return r.dbm.DB(ctx).Save(&entity).Error
+func (r *RepositoryImpl[T]) InsertBatch(ctx context.Context, entities []T, options ...DBOption) error {
+	return r.db(ctx, options...).Create(&entities).Error
 }
 
-func (r *RepositoryImpl[T]) SelectByID(ctx context.Context, id any, options ...QueryOption) (T, error) {
-	opt := buildQueryOptions(options...)
-	d := r.dbm.DB(ctx)
-	if opt.PreloadAssociations {
-		d = d.Preload(clause.Associations)
-	}
-
+func (r *RepositoryImpl[T]) Delete(ctx context.Context, condition T, options ...DBOption) error {
 	var entity T
-	err := d.First(&entity, id).Error
+	return r.db(ctx, options...).Where(&condition).Delete(&entity).Error
+}
+
+func (r *RepositoryImpl[T]) DeleteByID(ctx context.Context, id any, options ...DBOption) error {
+	var entity T
+	return r.db(ctx, options...).Delete(&entity, id).Error
+}
+
+func (r *RepositoryImpl[T]) DeleteByIDs(ctx context.Context, ids []any, options ...DBOption) error {
+	var entity T
+	return r.db(ctx, options...).Delete(&entity, ids).Error
+}
+
+func (r *RepositoryImpl[T]) Update(ctx context.Context, entity T, options ...DBOption) error {
+	return r.db(ctx, options...).Save(&entity).Error
+}
+
+func (r *RepositoryImpl[T]) UpdateBatch(ctx context.Context, entities []T, options ...DBOption) error {
+	return r.db(ctx, options...).Save(&entities).Error
+}
+
+func (r *RepositoryImpl[T]) SelectOne(ctx context.Context, condition T, options ...DBOption) (T, error) {
+	var entity T
+	err := r.db(ctx, options...).Where(&condition).First(&entity).Error
 	return entity, err
 }
-func (r *RepositoryImpl[T]) SelectByIDs(ctx context.Context, ids []any, options ...QueryOption) ([]T, error) {
-	opt := buildQueryOptions(options...)
-	d := r.dbm.DB(ctx)
-	if opt.PreloadAssociations {
-		d = d.Preload(clause.Associations)
-	}
 
+func (r *RepositoryImpl[T]) SelectList(ctx context.Context, condition T, options ...DBOption) ([]T, error) {
 	var entities []T
-	err := r.dbm.DB(ctx).Find(&entities, ids).Error
+	err := r.db(ctx, options...).Where(&condition).Find(&entities).Error
 	return entities, err
+}
+
+func (r *RepositoryImpl[T]) SelectByID(ctx context.Context, id any, options ...DBOption) (T, error) {
+	var entity T
+	err := r.db(ctx, options...).First(&entity, id).Error
+	return entity, err
+}
+
+func (r *RepositoryImpl[T]) SelectByIDs(ctx context.Context, ids []any, options ...DBOption) ([]T, error) {
+	var entities []T
+	err := r.db(ctx, options...).Find(&entities, ids).Error
+	return entities, err
+}
+
+func (r *RepositoryImpl[T]) SelectCount(ctx context.Context, condition T, options ...DBOption) (int64, error) {
+	var count int64
+	var model T
+	err := r.db(ctx, options...).Model(&model).Where(&condition).Count(&count).Error
+	return count, err
 }
--- crud/service.go
+++ crud/service.go
@@ -1,14 +1,22 @@
 package crud
 
-import "context"
+import (
+	"context"
+)
 
 type Service[T any] interface {
 	Save(ctx context.Context, entity T) error
-	RemoveByID(ctx context.Context, id any, options ...RemoveOption) error
-	RemoveByIDs(ctx context.Context, ids []any, options ...RemoveOption) error
-	UpdateByID(ctx context.Context, entity T) error
-	GetByID(ctx context.Context, id any, options ...QueryOption) (T, error)
-	ListByIDs(ctx context.Context, ids []any, options ...QueryOption) ([]T, error)
+	SaveBatch(ctx context.Context, entities []T) error
+	Remove(ctx context.Context, condition T) error
+	RemoveByID(ctx context.Context, id any) error
+	RemoveByIDs(ctx context.Context, ids []any) error
+	Update(ctx context.Context, entity T) error
+	UpdateBatch(ctx context.Context, entities []T) error
+	GetOne(ctx context.Context, condition T) (T, error)
+	List(ctx context.Context, condition T) ([]T, error)
+	GetByID(ctx context.Context, id any) (T, error)
+	ListByIDs(ctx context.Context, ids []any) ([]T, error)
+	Count(ctx context.Context, condition T) (int64, error)
 }
 
 type ServiceImpl[T any] struct {
@@ -19,22 +27,46 @@ func (s *ServiceImpl[T]) Save(ctx context.Context, entity T) error {
 	return s.repository.Insert(ctx, entity)
 }
 
-func (s *ServiceImpl[T]) RemoveByID(ctx context.Context, id any, options ...RemoveOption) error {
-	return s.repository.DeleteByID(ctx, id, options...)
+func (s *ServiceImpl[T]) SaveBatch(ctx context.Context, entities []T) error {
+	return s.repository.InsertBatch(ctx, entities)
 }
 
-func (s *ServiceImpl[T]) RemoveByIDs(ctx context.Context, ids []any, options ...RemoveOption) error {
-	return s.repository.DeleteByIDs(ctx, ids, options...)
+func (s *ServiceImpl[T]) Remove(ctx context.Context, condition T) error {
+	return s.repository.Delete(ctx, condition)
 }
 
-func (s *ServiceImpl[T]) UpdateByID(ctx context.Context, entity T) error {
-	return s.repository.UpdateByID(ctx, entity)
+func (s *ServiceImpl[T]) RemoveByID(ctx context.Context, id any) error {
+	return s.repository.DeleteByID(ctx, id)
 }
 
-func (s *ServiceImpl[T]) GetByID(ctx context.Context, id any, options ...QueryOption) (T, error) {
-	return s.repository.SelectByID(ctx, id, options...)
+func (s *ServiceImpl[T]) RemoveByIDs(ctx context.Context, ids []any) error {
+	return s.repository.DeleteByIDs(ctx, ids)
 }
 
-func (s *ServiceImpl[T]) ListByIDs(ctx context.Context, ids []any, options ...QueryOption) ([]T, error) {
-	return s.repository.SelectByIDs(ctx, ids, options...)
+func (s *ServiceImpl[T]) Update(ctx context.Context, entity T) error {
+	return s.repository.Update(ctx, entity)
+}
+
+func (s *ServiceImpl[T]) UpdateBatch(ctx context.Context, entities []T) error {
+	return s.repository.UpdateBatch(ctx, entities)
+}
+
+func (s *ServiceImpl[T]) GetOne(ctx context.Context, condition T) (T, error) {
+	return s.repository.SelectOne(ctx, condition)
+}
+
+func (s *ServiceImpl[T]) List(ctx context.Context, condition T) ([]T, error) {
+	return s.repository.SelectList(ctx, condition)
+}
+
+func (s *ServiceImpl[T]) GetByID(ctx context.Context, id any) (T, error) {
+	return s.repository.SelectByID(ctx, id)
+}
+
+func (s *ServiceImpl[T]) ListByIDs(ctx context.Context, ids []any) ([]T, error) {
+	return s.repository.SelectByIDs(ctx, ids)
+}
+
+func (s *ServiceImpl[T]) Count(ctx context.Context, condition T) (int64, error) {
+	return s.repository.SelectCount(ctx, condition)
 }
